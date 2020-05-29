41761e6f4cf6c66c7a5ead275bd3c994f869eb0e
DanteCV2 <dantecervega@gmail.com>
2020-05-28 21:27:51 -0500

    ok

--- goltweet client/src/utils/icons.js
+++
@@ -1,5 +0,0 @@
-export { ReactComponent as Location } from "../assets/svg/location.svg";
-export { ReactComponent as Calendar } from "../assets/svg/calendar.svg";
-export { ReactComponent as Link } from "../assets/svg/link.svg";
-export { ReactComponent as Close } from "../assets/svg/close.svg";
-export { ReactComponent as Camera } from "../assets/svg/camara.svg";
